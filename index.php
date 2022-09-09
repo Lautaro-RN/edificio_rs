42b616d37ec38beb2a32bf03eeb810bf495125f9
PLFG <lautarofranco@yahoo.com.ar>
2022-09-09 18:33:49 -0300

    version 9 del 9

--- index.php
+++ index.php
@@ -14,37 +14,37 @@
 		<?php include("nav_bs.html") ?>
 	</header>
 
-	<section id="nosotros">
+	<section id="nosotros"class="padding">
 		<div id="fotos_entrada">
-			<div class="sty_img_box" id="img1"><img class="img_pres" src="imgs/img1.png"></div>
-			<div class="sty_img_box" id="img2"><img class="img_pres" src="imgs/img2.png"></div>
+			<div class="sty_img_box" id="img1"><img class="img_pres"></div>
+			<div class="sty_img_box" id="img2"><img class="img_pres"></div>
 		</div>
-		<div id="txt_present">
+		<div id="txt_present" class="h4">
 			<p>Somos una empresa familiar con décadas de trayectoria en el rubro de la locación. Queremos que te sientas a gusto en tu próximo hogar, por eso nos ponemos a disposición para asistirte en la búsqueda de tu próxima vivienda.</p>
 			<p>En Edificio "Rama-Serka" ofrecemos un ambiente ordenado, agradable, seguro y práctico para nuestros clientes, un conjunto de cualidades al servicio de los huéspedes que hacen al disfrute de su estadía.</p>
 		</div>
 	</section>
 
-	<section id="servicios"><h2>Servicios</h2>
-		<div>
+	<section id="servicios"class="padding"><h2>Servicios</h2>
+		<div id="deps">
 			<div><h3>Departamentos</h3></div>
-			<div id="deps" class="img_servicios"></div>
+			<div id="img_deps" class="img_servicios"></div>
 		</div>
-		<div>
+		<div id="cochs">
 			<div><h3>Cocheras</h3></div>
-			<div id="cochs" class="img_servicios"></div>
+			<div id="img_cochs" class="img_servicios"></div>
 		</div>
-		<div>
+		<div id="local">
 			<div><h3>Locales</h3></div>
-			<div id="local" class="img_servicios"></div>
+			<div id="img_local" class="img_servicios"></div>
 		</div>
 	</section>
 
 	<section id="ubicacion"><h2>Donde estamos</h2>
-		<div id="mapa"><h3>En el mapa:</h3>
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d751.2736935507031!2d-71.30049831189172!3d-41.1324569885912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x961a7b70728b2a4f%3A0x660120e62d69e254!2s12%20de%20Octubre%20665%2C%20San%20Carlos%20de%20Bariloche%2C%20R%C3%ADo%20Negro!5e0!3m2!1ses-419!2sar!4v1630955143227!5m2!1ses-419!2sar" width="600" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+		<div id="mapa"class="divs_ubicacion"><h3>En el mapa:</h3>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d751.2736935507031!2d-71.30049831189172!3d-41.1324569885912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x961a7b70728b2a4f%3A0x660120e62d69e254!2s12%20de%20Octubre%20665%2C%20San%20Carlos%20de%20Bariloche%2C%20R%C3%ADo%20Negro!5e0!3m2!1ses-419!2sar!4v1630955143227!5m2!1ses-419!2sar" width="420"height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 			</div>
-		<div id="distancias">
+		<div id="distancias"class="divs_ubicacion">
 			<div><h2>Distancias cercanas</h2></div>
 			<?php include("accordion_dist.html");?>
  		</div>
